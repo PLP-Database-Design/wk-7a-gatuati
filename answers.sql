9e85cd944a819b33b5efaf5517235140d1f07168
gatuati <125788606+gatuati@users.noreply.github.com>
2025-04-15 23:03:17 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,18 @@
+1. 
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+2.
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
